23084e4db7ef83a2f3abeee21ce0e0c06fd9dfcd
aaron <amondel2@gmail.com>
2018-04-28 20:44:58 -0400

    Add two new features 1. Add More Testing 2. Fix it so the correct title is displayed.

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,6 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 
 waiting {
     timeout = 2
@@ -25,6 +26,23 @@ environments {
 //    // run via “./gradlew -Dgeb.env=firefox iT”
     firefox {
         atCheckWaiting = 1
-        driver = { new FirefoxDriver() }
+
+        driver = {
+            FirefoxOptions o = new FirefoxOptions()
+            o.addArguments('screenshot')
+            new FirefoxDriver()
+
+        }
+    }
+
+    firefoxHeadless {
+        atCheckWaiting = 1
+
+        driver = {
+            FirefoxOptions o = new FirefoxOptions()
+            o.addArguments(['screenshot','headless'])
+            new FirefoxDriver()
+
+        }
     }
 }
